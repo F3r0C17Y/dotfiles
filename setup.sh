57e59ae66d574f15fa3dd78032cbeda0d9139ca4
Ferocity <christian.fries123@gmail.com>
2015-11-30 19:31:19 +0100

    kick the tires

--- setup.sh
+++ setup.sh
@@ -8,10 +8,14 @@ mkdir -p $BACKUP
 for file in $PWD/*
 do
     filename=$(basename $file)
-    if [[ "$filename" != "$(basename $0)" ]]; then
+    if [ "$filename" != "$(basename $0)" ] && [ ! -d "${filename}" ]; then
         mv -f $HOME/.$filename $BACKUP/
         ln -s $PWD/$filename $HOME/.$filename
     fi
 done
 
+ln -sf $PWD/vim $HOME/.vim
+
 git submodule init && git submodule update
+
+ln -sf $PWD/vim/autoload/vim-pathogen/autoload/pathogen.vim $PWD/vim/autoload/pathogen.vim 
