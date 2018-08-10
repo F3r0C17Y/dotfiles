0fd244f6ea13048da4b5231e4c6fbabbd2663870
Ferocity <christian.fries123@gmail.com>
2018-08-10 13:28:14 +0200

    add parameter to setup to update vim and pwndbg progress bar

--- setup.sh
+++ setup.sh
@@ -2,13 +2,25 @@
 
 PWD=$(pwd)
 BACKUP=$PWD/backup
+update=false
+
+progress_steps=1
+
+
+function progressBar() 
+{
+    local progressBar="#"
+    for i in $(seq $progress_steps); do
+        printf "%s" "${progressBar}"
+    done
+}
 
 git_checkout()
 {
     DEST=$1
     REPO=$2
     REV=$3
-
+    (
     if [[ ! -e "$DEST/.git" ]]; then
         git clone $REPO $DEST
     else
@@ -20,8 +32,45 @@ git_checkout()
         git checkout $REV
         popd
     fi
+    ) | progressBar
 }
 
+init_vim_plugins()
+{
+    echo -ne "[*] init vim plugins ["
+    progress_steps=$(expr 80 / 11)
+    mkdir -p ${PWD}/vim/autoload
+    mkdir -p ${PWD}/vim/bundle
+    git_checkout vim/autoload/vim-pathogen https://github.com/tpope/vim-pathogen.git master
+    git_checkout vim/bundle/nerdtree https://github.com/scrooloose/nerdtree.git master
+    git_checkout vim/bundle/vim-airline https://github.com/bling/vim-airline.git master
+    git_checkout vim/bundle/vim-airline-themes https://github.com/vim-airline/vim-airline-themes master
+    git_checkout vim/bundle/vim-fugitive https://github.com/tpope/vim-fugitive.git master
+    git_checkout vim/bundle/vim-bufferline https://github.com/bling/vim-bufferline.git master
+    git_checkout vim/bundle/cscope-maps https://github.com/joe-skb7/cscope-maps.git master
+    git_checkout vim/bundle/vim-tagbar https://github.com/majutsushi/tagbar.git master
+    git_checkout vim/bundle/vim-better-whitespace https://github.com/ntpeters/vim-better-whitespace.git master
+    git_checkout powerline/fonts https://github.com/powerline/fonts.git master
+
+    if [ ! -e $PWD/vim/autoload/pathogen.vim ]; then
+	    ln -sf $PWD/vim/autoload/vim-pathogen/autoload/pathogen.vim $PWD/vim/autoload/pathogen.vim
+    fi
+
+    pushd powerline/fonts/ > /dev/null
+    ./install.sh | progressBar
+    popd > /dev/null
+    echo "]"
+}
+
+init_pwn_dbg()
+{
+    echo "[*] init pwndbg"
+    git_checkout pwndbg https://github.com/pwndbg/pwndbg.git stable
+
+    pushd pwndbg
+    ./setup.sh
+    popd
+}
 
 #######################################
 # MAIN
@@ -30,6 +79,18 @@ if [ ! -e "$PWD/setup.sh" ]; then
     echo "Please start setup.sh inside dotfile repo root!"
     exit -1
 fi
+
+
+while getopts "u" opt; do
+  case ${opt} in
+    u )
+      update=true
+      ;;
+    \? ) echo "Usage: setup.sh [-u]"
+      ;;
+  esac
+done
+
 
 echo "Make backups and create dotfiles"
 mkdir -p $BACKUP
@@ -47,28 +108,13 @@ if [ ! -e $HOME/.vim ]; then
 fi
 
 echo "Init git modules"
-mkdir -p ${PWD}/vim/autoload
-mkdir -p ${PWD}/vim/bundle
-git_checkout pwndbg https://github.com/pwndbg/pwndbg.git stable
-git_checkout vim/autoload/vim-pathogen https://github.com/tpope/vim-pathogen.git master
-git_checkout vim/bundle/nerdtree https://github.com/scrooloose/nerdtree.git master
-git_checkout vim/bundle/vim-airline https://github.com/bling/vim-airline.git master
-git_checkout vim/bundle/vim-airline-themes https://github.com/vim-airline/vim-airline-themes master
-git_checkout vim/bundle/vim-fugitive https://github.com/tpope/vim-fugitive.git master
-git_checkout vim/bundle/vim-bufferline https://github.com/bling/vim-bufferline.git master
-git_checkout vim/bundle/cscope-maps https://github.com/joe-skb7/cscope-maps.git master
-git_checkout vim/bundle/vim-tagbar https://github.com/majutsushi/tagbar.git master
-git_checkout vim/bundle/vim-better-whitespace https://github.com/ntpeters/vim-better-whitespace.git master
-git_checkout powerline/fonts https://github.com/powerline/fonts.git master
 
-if [ ! -e $PWD/vim/autoload/pathogen.vim ]; then
-	ln -sf $PWD/vim/autoload/vim-pathogen/autoload/pathogen.vim $PWD/vim/autoload/pathogen.vim
+
+if [ ! -e ${PWD}/vim/autoload ] || [ $update = "true" ]; then
+    init_vim_plugins
 fi
 
-pushd powerline/fonts/
-./install.sh
-popd
-
-pushd pwndbg
-./setup.sh
-popd
+exit;
+if [ ! -e ${PWD}/pwndbg ] || [ $update = "true" ]; then
+    init_pwn_dbg
+fi
