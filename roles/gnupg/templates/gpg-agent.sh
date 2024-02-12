b24433d4ea858ef491ad5fe788029762df889aff
Christian Fries <christian.fries123@gmail.com>
2024-02-12 17:27:47 +0100

    Update roles

--- roles/gnupg/templates/gpg-agent.sh
+++ roles/gnupg/templates/gpg-agent.sh
@@ -1,3 +1,7 @@
+#export GPG_TTY="$(tty)"
+#export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+#gpg-connect-agent updatestartuptty /bye >/dev/null
+
 export GPG_TTY="$(tty)"
-export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
-gpg-connect-agent updatestartuptty /bye >/dev/null
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
